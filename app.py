03714d3215d3172378ac2860211891378c3b0ec8
webert12 <56725537+webert12@users.noreply.github.com>
2026-08-11 14:40:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def enviar_notificacao_automatica_termux(nome_cliente, telefone_cliente, data_fo
 
     link_wa_cliente = f"https://wa.me/{num_cliente_limpo}" if num_cliente_limpo else "Não informado"
 
-    url_api = "https://d778198417a791.lhr.life/enviar"
+    url_api = "https://17822710a0880b.lhr.life/enviar"
     payload = {
         "telefone": telefone_dono,
         "mensagem": (
